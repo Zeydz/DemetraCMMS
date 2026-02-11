e692b3963ec1d69910f03242a2a8b9a8472efd15
Jocke <joakim03moller@gmail.com>
2026-02-11 10:22:25 +0100

    feat: public navbar implemented

---
+++ Views/Shared/_LayoutPublic.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotnet_projektuppgift.Data.Views.Shared;
+
+public class _LayoutPublic : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
