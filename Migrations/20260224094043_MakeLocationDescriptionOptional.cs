2273d728bc512ff262f6c17c7811696cda51266b
Jocke <joakim03moller@gmail.com>
2026-02-24 11:35:20 +0100

    feat: implemented details-view for specific location. New migration aswell

---
+++ Migrations/20260224094043_MakeLocationDescriptionOptional.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_projektuppgift.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeLocationDescriptionOptional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Locations",
+                type: "TEXT",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 500);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Locations",
+                type: "TEXT",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+    }
+}
